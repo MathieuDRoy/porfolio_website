e6fe68d13037d68d4c1d3310fb462385db87883d
MathieuDRoy <mathieuroy1498@gmail.com>
2022-10-06 13:25:54 -0400

    model appears in window with colour

--- js/example8.js
+++ js/example8.js
@@ -1,16 +1,13 @@
 var scene, camera, renderer, cube, controls, sculpture;
 
-
-
 function init(v_output) {
-
-
 
     scene = new THREE.Scene();
     camera = new THREE.PerspectiveCamera(80, window.innerWidth / window.innerHeight, 0.1, 1000);
 
     renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setClearColor( 0xffffff ); // white background
     document.body.appendChild(renderer.domElement);
     
     controls = new THREE.OrbitControls( camera, renderer.domElement );
@@ -19,19 +16,37 @@ function init(v_output) {
     
     var distanceforcam = 2*100;
 
-    const geometry = new THREE.SphereGeometry(3, 20, 20);
+    const geometry = new THREE.SphereGeometry(2.7, 20, 20);
 
-    const material = new THREE.MeshBasicMaterial( { color: "#7A7AAF"} );
+    const material1 = new THREE.MeshBasicMaterial( { color: "#FF0000"} );
+    const material2 = new THREE.MeshBasicMaterial( { color: "#0000FF"} );
+    const material3 = new THREE.MeshBasicMaterial( { color: "#00FF00"} );
     var particles = new Array();
     var threeD = new Array();
     threeD = cleanArray(v_output);
+    var x = 0;
+    var y = 0;
+    var z = 0;
 
-    for (let allParticles = 10; allParticles < threeD.length; allParticles += 5) {
+    //console.log(threeD);
+
+
+
+    for (let allParticles = 9; allParticles < threeD.length; allParticles += 6) {
         particles[allParticles] = new Array();
-        particles[allParticles] = new THREE.Mesh( geometry, material );
+        console.log(allParticles);
+        if (threeD[allParticles + 1] == "0"){
+            particles[allParticles] = new THREE.Mesh( geometry, material1 );
+        } else if (threeD[allParticles + 1] == "1"){
+            particles[allParticles] = new THREE.Mesh( geometry, material2 );
+        } else {
+            particles[allParticles] = new THREE.Mesh( geometry, material3 );
+        }
+        
         scene.add(particles[allParticles]);
         particles[allParticles].position.set(threeD[allParticles+2], threeD[allParticles+3], threeD[allParticles+4]);
     }
+
 
     camera.position.z = distanceforcam;
     
