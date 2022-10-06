14c56811c8333aed289a0a1879efe45538d3c1b2
MathieuDRoy <mathieuroy1498@gmail.com>
2022-10-06 08:37:15 -0400

    file reading works. modify array to be usefull

--- js/example8.js
+++ js/example8.js
@@ -1,8 +1,10 @@
 var scene, camera, renderer, cube, controls, sculpture;
-init();
-animate();
 
-function init() {
+
+
+function init(v_output) {
+
+
 
     scene = new THREE.Scene();
     camera = new THREE.PerspectiveCamera(80, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -21,27 +23,17 @@ function init() {
 
     const material = new THREE.MeshBasicMaterial( { color: "#7A7AAF"} );
     var threeD = new Array();
+
+    threeD = v_output.split(' ');
+    alert(threeD);
+
     
-    //threeD = readFile();
-
-    const fileList = new Array();
-    let fileContent = "";
-
-    const fr = new FileReader();
-    fr.onload = () => {
-    fileContent = fr.result;
-    console.log('Commands', fileContent);
-    }
-
-    fr.readAsText(fileList[0]);
-
-    console.log(threeD[1]);
-
     camera.position.z = distanceforcam;
     
     controls.enablePan = false;
     controls.enableDamping = true;   
 
+    animate();
 }
 
 function animate() {
@@ -56,19 +48,5 @@ function onWindowResize() {
     renderer.setSize( window.innerWidth, window.innerHeight );
 }
 
-async function readFile() {
-    try {
-        const contents = await fsPromises.readFile(basic.pos, 'utf-8');
-    
-        const arr = contents.split(/\r?\n/);
-    
-        console.log(arr);
-        
-    
-        return arr;
-      } catch (err) {
-        console.log(err);
-      }
-}
 
 window.addEventListener('resize', onWindowResize);
