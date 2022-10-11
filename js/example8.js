c26cdf158d3dfd5a874f1c9424364e23057b418f
MathieuDRoy <mathieuroy1498@gmail.com>
2022-10-11 14:26:21 -0400

    colour picker, draw box works, othographic vw nxt

--- js/example8.js
+++ js/example8.js
@@ -1,24 +1,21 @@
 // for colour picking https://threejs.org/examples/#webgl_materials_car
 
 var scene, camera, renderer, controls;
-var vDrawBox = true;
+var vDrawBox = false;
 var threeD = new Array();
+var perspective;
 
 function init(v_output) {
-
-    scene = new THREE.Scene();
-    camera = new THREE.PerspectiveCamera(80, window.innerWidth / window.innerHeight, 0.1, 1000);
-
+    scene = new THREE.Scene();    
     renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.setClearColor( 0xffffff ); // white background
     document.body.appendChild(renderer.domElement);
-
+    camera = new THREE.PerspectiveCamera(80, window.innerWidth / window.innerHeight, 0.1, 1000);
     controls = new THREE.OrbitControls( camera, renderer.domElement );
     //controls.autoRotate = true;
     controls.update();
 
-    
     threeD = cleanArray(v_output);
     drawBox();
     drawModel();
@@ -32,6 +29,20 @@ function init(v_output) {
 }
 
 function animate() {
+
+    /*if (perspective == 1) {
+        camera = new THREE.PerspectiveCamera(80, window.innerWidth / window.innerHeight, 0.1, 1000);
+        controls = new THREE.OrbitControls( camera, renderer.domElement );
+        controls.update();
+        camera.position.z = threeD[0];
+        console.log("no");
+    } else if (perspective == 2) {
+        camera = new THREE.OrthographicCamera(-window.innerWidth, window.innerWidth,  window.innerWidth / -2,  window.innerWidth / 2, 0.1, 1000);
+        controls = new THREE.OrbitControls( camera, renderer.domElement );
+        controls.update();
+        camera.position.z = threeD[0];
+        console.log("yes");
+    }*/
     controls.update();
     renderer.render(scene, camera);
     window.requestAnimationFrame(animate);
@@ -117,20 +128,44 @@ function drawModel() {
 
     const geometry = new THREE.SphereGeometry(document.getElementById("radius").value, document.getElementById("slice").value, document.getElementById("stack").value);
 
-    const material1 = new THREE.MeshBasicMaterial( { color: "#FF0000"} );
-    const material2 = new THREE.MeshBasicMaterial( { color: "#0000FF"} );
-    const material3 = new THREE.MeshBasicMaterial( { color: "#00FF00"} );
+    const material0 = new THREE.MeshBasicMaterial( { color: document.getElementById("zeroColour").value} );
+    const material1 = new THREE.MeshBasicMaterial( { color: document.getElementById("oneColour").value} );
+    const material2 = new THREE.MeshBasicMaterial( { color: document.getElementById("twoColour").value} );
+    const material3 = new THREE.MeshBasicMaterial( { color: document.getElementById("threeColour").value} );
+    const material4 = new THREE.MeshBasicMaterial( { color: document.getElementById("fourColour").value} );
+    const material5 = new THREE.MeshBasicMaterial( { color: document.getElementById("fiveColour").value} );
+    const material6 = new THREE.MeshBasicMaterial( { color: document.getElementById("sixColour").value} );
+    const material7 = new THREE.MeshBasicMaterial( { color: document.getElementById("sevenColour").value} );
+    const material8 = new THREE.MeshBasicMaterial( { color: document.getElementById("eightColour").value} );
+    const material9 = new THREE.MeshBasicMaterial( { color: document.getElementById("nineColour").value} );
+    const material10 = new THREE.MeshBasicMaterial( { color: "#000000"} );
 
     var particles = new Array();
 
     for (let allParticles = 9; allParticles < threeD.length; allParticles += 6) {
         particles[allParticles] = new Array();
         if (threeD[allParticles + 1] == "0"){
-            particles[allParticles] = new THREE.Mesh( geometry, material1 );
+            particles[allParticles] = new THREE.Mesh( geometry, material0 );
         } else if (threeD[allParticles + 1] == "1"){
+            particles[allParticles] = new THREE.Mesh( geometry, material1 );
+        } else if (threeD[allParticles + 1] == "2"){
             particles[allParticles] = new THREE.Mesh( geometry, material2 );
-        } else {
+        } else if (threeD[allParticles + 1] == "3"){
             particles[allParticles] = new THREE.Mesh( geometry, material3 );
+        } else if (threeD[allParticles + 1] == "4"){
+            particles[allParticles] = new THREE.Mesh( geometry, material4 );
+        } else if (threeD[allParticles + 1] == "5"){
+            particles[allParticles] = new THREE.Mesh( geometry, material5 );
+        } else if (threeD[allParticles + 1] == "6"){
+            particles[allParticles] = new THREE.Mesh( geometry, material6 );
+        } else if (threeD[allParticles + 1] == "7"){
+            particles[allParticles] = new THREE.Mesh( geometry, material7 );
+        } else if (threeD[allParticles + 1] == "8"){
+            particles[allParticles] = new THREE.Mesh( geometry, material8 );
+        } else if (threeD[allParticles + 1] == "9"){
+            particles[allParticles] = new THREE.Mesh( geometry, material9 );
+        } else {
+            particles[allParticles] = new THREE.Mesh( geometry, material10 );
         }
         scene.add(particles[allParticles]);
         particles[allParticles].position.set(threeD[allParticles+2]-51, threeD[allParticles+3]-51, threeD[allParticles+4]-51);
@@ -138,6 +173,14 @@ function drawModel() {
     }
 
     drawBox();
+}
+
+function setCameraView() {
+    if (document.getElementById("setCamera") == "on"){
+        perspective = 1;
+    } else {
+        perspective = 2;
+    }
 }
 
 
